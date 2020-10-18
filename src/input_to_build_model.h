32f3e8dc64650dd8ce84c6d0bb9d8ba82c82f87c
rohit@linux <rohitchowdhury@iisc.ac.in>
2020-10-18 16:03:02 +0530

    [wip] running comparison with stream implementation

--- src/input_to_build_model.h
+++ src/input_to_build_model.h
@@ -1,8 +1,8 @@
 //Prob type: time, energy1, energy2, energy3
-std::string prob_type = "energy1"; //verify num_ac_speeds 
-std::string prob_name = "AF_DG_g200x200x50_r100";
+std::string prob_type = "time"; //verify num_ac_speeds 
+std::string prob_name = "AF_DG_g200x200x3_r1k_profTest";
 
-int32_t nt = 50;
+int32_t nt = 3;
 float dt = 1;
 int32_t gsize = 200;
 float dx = 80.0/gsize; float dy = dx;
@@ -11,11 +11,11 @@ float x0 = dx/2;
 float y0 = dy/2;
 
 float F = 1.2;
-int num_ac_speeds = 2; //verify prob_type
+int num_ac_speeds = 1; //verify prob_type
 int num_ac_angles = 16;
 int32_t num_actions = num_ac_speeds*num_ac_angles;
 
-int32_t nrzns = 100;     // verify with probname
+int32_t nrzns = 1000;     // verify with probname
 int32_t bDimx = nrzns;
 
 float r_outbound = -1000;
